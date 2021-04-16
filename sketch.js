517217c710e902281045666b54ca5978107c8b9a
Kourosh Simpkins <k.simpkins0820201@arts.ac.uk>
2021-04-16 11:07:29 +0100

    Added comments
    added comments for a task in foundational methods for creative computing 2

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 /* jshint esversion: 6 */
+/* AHHH THIS ENTIRE TIME I'VE BEEN SPELLING POINTILLISM WRONG */
 
 let img;
 let smallPoint, largePoint;
@@ -8,21 +9,22 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(img.width, img.height);
-  frameRate(500);
+  createCanvas(img.width, img.height); //this creates a canvas with the same dimensions as the original image
+  frameRate(144); //change the framerate here if you want to try and speed up or slowdown the rate that the Pointillism is done
   smallPoint = 1;
   largePoint = 10;
   imageMode(CENTER);
-  noStroke();
-  background(255);
-  img.loadPixels();
+  noStroke(); //We don't want any pesky outlines around our circles do we?
+  background(255); //Plain background, simple.
+  img.loadPixels(); //We don't want do display the image itself, we want the colour of the pixels, so we need to prepare them.
 }
 
 function draw() {
+  // basically runs through to get a random pixel and then draw an ellipse from that point.
   let points = random(smallPoint, largePoint);
   let x = floor(random(img.width));
   let y = floor(random(img.height));
-  let pix = img.get(x, y);
+  let pix = img.get(x, y); //get the value of the pixel at a specific x y coord in the original image.
   fill(pix, 128);
   ellipse(x, y, points, points);
 }
